281a19cfc31bebbbaae69c9b9642550e81e1398d
Zackary Zielinski <161618228+P135928@users.noreply.github.com>
2025-01-22 17:29:47 -0700

    Update gamellb.h
    Made a random number generator, this is going to be used to make decisions and remove things. EG health.

--- gamellb.h
+++ gamellb.h
@@ -1,12 +1,21 @@
 #include <iostream>
-#include <cstdlib>
-#include <ctime>
+#include <random>
 using namespace std;
 
-srand(time(nullptr));
-int health;
-int randomNumber = rand();
+int health = 100;
 
 int part1() {
-	cout << 
+	random_device rd;
+    mt19937 gen(rd());
+
+    // Define the range
+    int min = 1;
+    int max = 10;
+    uniform_int_distribution<> distrib(min, max);
+    for (int i = 0; i < 1; ++i) {
+    int randomNumber = distrib(gen);
+    cout << "Random number " << i + 1 << ": " << randomNumber << endl;
+    cout << health - randomNumber;
+	}
+	return 0;
 } 
