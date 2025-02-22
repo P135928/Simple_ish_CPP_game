c2a2b408b7cf29a2603fa53cdd910134ced58d78
P135928 <zackaryzpi@gmail.com>
2025-02-22 10:01:05 -0700

    Added Death, it shows the absoulute value of your health after death. Fixed some more bugs with the program closing immediately.

--- gamelib.h
+++ gamelib.h
@@ -3,11 +3,16 @@
 #include <ctime>
 using namespace std;
 // Makes a function with 2 paramaters health, maxNumber
-int part1(int health, int maxNum) {
+int healthSub(int health, int maxNum) {
 	// Get a different random number each time the program runs.
 	srand(time(0));
 	// Actually makes the random number.
 	int randNum = rand() % maxNum;
 	// Removes the randNum from health.
-	return health - randNum;
+	int newHealth = health - randNum;
+	if (newHealth <= 0) {
+		cout << "You died!\n";
+	}
+	cout << abs(newHealth) << "\n";
+	return 0;
 }
--- main.cpp
+++ main.cpp
@@ -3,7 +3,33 @@
 #include "gamelib.h"
 using namespace std;
 
+int health;
+int maxNum;
+int charater;
+
 int main() {
+	// Asks the user for what they wanna be.
+	cout << "Who do you want to be? \n";
+	cout << "1. A warrior\n";
+	cout << "2. A wizard\n";
+	cout << "3. A thief\n";
+	cout << "4. A ranger\n";
+	cin >> charater;
+	switch(charater) {
+		case 1:
+			health = 100;
+			break;
+		case 2:
+			health = 50;
+			break;
+		case 3:
+			health = 25;
+			break;
+		case 4:
+			health = 75;
+			break;
+	}
 	// Calls the function with 2 paramaters. Health and maxNumber
-	part1(1000, 230);
+	healthSub(health, 50);
+	system("pause");
 }
