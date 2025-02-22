0f552611e5af135429c344b045e81a35d0e435f0
P135928 <zackaryzpi@gmail.com>
2025-02-22 08:21:20 -0700

    It wasn't compiling but I fixed it! Also fixed some issues in my code. Comes with a precompiled version of it too.

--- gamelib.h
+++ gamelib.h
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <cstdlib>
+#include <ctime>
 using namespace std;
 // Makes a function with 2 paramaters health, maxNumber
 int part1(int health, int maxNum) {
 	// Get a different random number each time the program runs.
 	srand(time(0));
-	//Actually makes the random number.
+	// Actually makes the random number.
 	int randNum = rand() % maxNum;
-	//Removes the randNum from health.
-	cout << health - randNum;
+	// Removes the randNum from health.
+	return health - randNum;
 }
